833417abc1569a2259d3f28896761dbaa3bde772
amotz <mochida.atsushi@classmethod.jp>
2023-02-13 18:00:40 +0900

    install tiptap

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <client-only>
+      <tiptap-editor />
+    </client-only>
   </div>
 </template>
+<script>
+import TiptapEditor from '~/components/TiptapEditor.vue'
+export default {
+  components: {
+    TiptapEditor
+  }
+}
+</script>
---
+++ components/TiptapEditor.vue
@@ -0,0 +1,76 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: '',
+      extensions: [
+        StarterKit,
+      ],
+    })
+  },
+
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
+
+<style lang="scss">
+/* Basic editor styles */
+.ProseMirror {
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  border: 1px solid #e0e0e0 !important;
+  box-sizing: border-box;
+  border-radius: 3px;
+  padding: 16px 16px 16px 16px;
+
+  font-size: 14px;
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+}
+
+.content {
+  padding: 1rem 0 0;
+
+  h3 {
+    margin: 1rem 0 0.5rem;
+  }
+
+  pre {
+    border-radius: 5px;
+    color: #333;
+  }
+
+  code {
+    display: block;
+    white-space: pre-wrap;
+    font-size: 0.8rem;
+    padding: 0.75rem 1rem;
+    background-color:#e9ecef;
+    color: #495057;
+  }
+}
+</style>
